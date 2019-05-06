789e8f951dd3344b6e6ed587852b6fcb647b37a0
zhuangxuchi <1617225642@qq.com>
2019-05-06 22:53:03 +0800

    Create 917. 仅仅反转字母.js

---
+++ 917. 仅仅反转字母.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var reverseOnlyLetters = function(S) {
+    s=S.split('');
+    let a=0;
+    let c=[];
+    let d=[];
+    for(let i=S.length-1;i>=0;i--){
+        if((s[i].charCodeAt()>=65&&s[i].charCodeAt()<=90)||(s[i].charCodeAt()>=97&&s[i].charCodeAt()<=122)){
+            c.push(s[i])
+        }else{
+            d[a]=[i,s[i]];
+            a++;
+        }
+    }
+    for(let j=d.length-1;j>=0;j--){
+       c.splice(d[j][0],0,d[j][1])
+    }return c.join('');
+};
+console.log(reverseOnlyLetters("7_28]"))
