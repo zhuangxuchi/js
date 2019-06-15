d3ec84124e8c684379736687cd3bc66772e9ff81
zhuangxuchi <1617225642@qq.com>
2019-06-15 22:20:04 +0800

    Create 299. 猜数字游戏.js

---
+++ 299. 猜数字游戏.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+var getHint = function(secret, guess) {
+    let a=0;
+    let b=0;
+    let s=secret.split('');
+    let g=guess.split('');
+    for(let i=0;i<g.length;i++){
+        if(s[i]==g[i]){
+            a++;
+            s.splice(i,1);
+            g.splice(i,1);
+            i--;
+        }
+    };
+    for(let i=0;i<s.length;i++){
+        for(let j=0;j<g.length;j++){
+            if(s[i]==g[j]){
+                b++;
+                s.splice(i,1);
+                g.splice(j,1);
+                i--;
+                j--;
+            }
+        }
+    }return a.toString()+'A'+b.toString()+'B'
+};
+console.log(getHint('11','11'))
