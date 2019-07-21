2177c62f13f6de8040ae5fc02f49ffec06a83704
zhuangxuchi <1617225642@qq.com>
2019-07-21 19:28:09 +0800

    Update 1002. 查找常用字符.js

--- 1002. 查找常用字符.js
+++ 1002. 查找常用字符.js
@@ -3,19 +3,18 @@
  * @return {string[]}
  */
 var commonChars = function(A) {
-    let arr = A[0].split('')
-    for(let i = 1; i < A.length; i++){
-        let arr1 = A[i].split('')
-        let arr2 = []
-        arr1.forEach(function(str){
-            let num = arr.indexOf(str)
-            if(num >= 0){
-                arr2.push(str)
-                arr.splice(num, 1)
+    let arr = A[0].split('');
+    let a;
+    for(let i = 1,len=A.length; i < len; i++){
+        let arr1 = A[i].split('');
+        let arr2 = [];
+        for(let j=0,len=arr1.length;j<len;j++){
+            a=arr.indexOf(arr1[j]);
+            if(a!=-1){
+                arr2.push(arr1[j]);
+                arr.splice(a,1)
             }
-        })
-        arr = arr2
-    }
-    return arr
+        }
+        arr = arr2;
+    }return arr
 };
-console.log(commonChars(["bella","label","roller"]))
