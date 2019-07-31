641b7774dc599509d117442f6b45bc1abc49d95f
zhuangxuchi <1617225642@qq.com>
2019-08-01 01:24:39 +0800

    Update 994. 腐烂的橘子.js

--- 994. 腐烂的橘子.js
+++ 994. 腐烂的橘子.js
@@ -3,56 +3,43 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    let a=0;
-    for(let i=0;i<grid.length;i++){
-        if(grid[i].indexOf(1)!=-1){
-            a=1;
-            break;
+    let height=grid.length;
+    let width=grid[0].length;
+    let oranges=[];
+    let dirs=[[1, 0], [-1, 0], [0, 1], [0, -1]];
+    let good=0;
+    for(let i=0;i<height;i++){
+        for(let j=0;j<width;j++){
+            if(grid[i][j]==2){
+                oranges.push([i,j]);
+            }else if(grid[i][j]==1){
+                good++;
+            }
         }
     }
-    if(a==0){
-        return 0
+    if(good==0){
+        return 0;
     }
-    let res=0;
-    while(1){
-        let j=0;
-        let oranges=[];
-        for(let i=0;i<grid.length;i++){
-            for(let k=0;k<grid[i].length;k++){
-                if(grid[i][k]==2){
-                    oranges.push([i,k])
-                }
+    let temp,x,y;
+    let time=0;
+    let que=[];
+    while(oranges.length>0){
+        temp=oranges.pop();
+        for(let i=0;i<4;i++){
+            x=temp[0];
+            y=temp[1];
+            x+=dirs[i][0];
+            y+=dirs[i][1];
+            if(x>=0 && x<height && y>=0 && y<width && grid[x][y]==1){
+                que.push([x,y]);
+                grid[x][y]=2;
+                good--;
             }
         }
-        for(let i=0;i<oranges.length;i++){
-            let x=oranges[i][0];
-            let y=oranges[i][1];
-            if(grid[x+1]!==undefined && grid[x+1][y]==1){
-                grid[x+1][y]=2;
-                j=1;
-            }
-            if(grid[x]!==undefined && grid[x][y+1]==1){
-                grid[x][y+1]=2;
-                j=1;
-            }
-            if(grid[x-1]!==undefined && grid[x-1][y]==1){
-                grid[x-1][y]=2;
-                j=1;
-            }
-            if(grid[x]!==undefined && grid[x][y-1]==1){
-                grid[x][y-1]=2;
-                j=1;
-            }
+        if(oranges.length==0){
+            oranges=que;
+            que=[];
+            time++;
         }
-        if(j==1){
-            res++;
-        }else{
-            break;
-        }
-    }
-    for(let i=0;i<grid.length;i++){
-        if(grid[i].indexOf(1)!=-1){
-            return -1
-        }
-    }return res
-};
+    }return good==0 ? time-1:-1
+}
