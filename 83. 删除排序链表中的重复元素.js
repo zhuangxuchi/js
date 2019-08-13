418ee4902d58a351fa53824b099b488328518281
zhuangxuchi <1617225642@qq.com>
2019-08-14 01:15:56 +0800

    Create 83. 删除排序链表中的重复元素.js

---
+++ 83. 删除排序链表中的重复元素.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let curr=head;
+    while(curr!==null && curr.next!==null){
+        if(curr.val==curr.next.val){
+            curr.next=curr.next.next;
+        }else{
+            curr=curr.next;
+        }
+    }return head;
+};
