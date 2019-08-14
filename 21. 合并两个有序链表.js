d8758783097553faa02113e12b506eb461d0aa84
zhuangxuchi <1617225642@qq.com>
2019-08-15 01:05:09 +0800

    Update 21. 合并两个有序链表.js

--- 21. 合并两个有序链表.js
+++ 21. 合并两个有序链表.js
@@ -11,17 +11,38 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    if(l1==null){
+    if(l1==null && l2!==null){
         return l2;
-    }
-    if(l2==null){
+    }else if(l2==null && l1!==null){
+        return l1;
+    }else if(l1==null && l2==null){
         return l1;
     }
+    let curr,head;
     if(l1.val<l2.val){
-        l1.next=mergeTwoLists(l1.next,l2);
-        return l1;
+        curr=l1;
+        head=curr;
+        l1=l1.next;
     }else{
-        l2.next=mergeTwoLists(l1,l2.next);
-        return l2
+        curr=l2;
+        head=curr;
+        l2=l2.next;
     }
+    while(l1!==null && l2!==null){
+        if(l1.val<l2.val){
+            curr.next=l1;
+            curr=curr.next;
+            l1=l1.next;
+        }else{
+            curr.next=l2;
+            curr=curr.next;
+            l2=l2.next;
+        }
+    }
+    if(l1==null){
+        curr.next=l2;
+    }else{
+        curr.next=l1;
+    }
+    return head;
 };
