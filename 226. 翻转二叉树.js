2d2883897547f5889bc10d0476299df2316892fc
zhuangxuchi <1617225642@qq.com>
2019-08-19 00:08:40 +0800

    Update 226. 翻转二叉树.js

--- 226. 翻转二叉树.js
+++ 226. 翻转二叉树.js
@@ -10,5 +10,16 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    
+    if(root==null){
+        return root
+    }
+    if(root.left==null && root.right==null){
+        return root
+    }
+    let a=root.left;
+    root.left=root.right;
+    root.right=a;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root
 };
