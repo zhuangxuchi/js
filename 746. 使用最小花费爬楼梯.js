1cc036ef413070538ed7b16bf259ab2696ba2401
zhuangxuchi <1617225642@qq.com>
2019-08-27 23:54:40 +0800

    Update 746. 使用最小花费爬楼梯.js

--- 746. 使用最小花费爬楼梯.js
+++ 746. 使用最小花费爬楼梯.js
@@ -7,21 +7,14 @@ var minCostClimbingStairs = function(cost) {
     cost.push(0);
     cost.unshift(0);
     cost.unshift(0);
-    let m=new Map();
-    let opt=function(x){
-        if(x==0){
-            return cost[0];
-        }else if(x==1){
-            return Math.min(cost[0],cost[1]);
-        }else{
-            if(!m.has(x)){
-                let a=opt(x-1)+cost[x];
-                let b=opt(x-2)+cost[x];
-                let c=Math.min(a,b);
-                m.set(x,c)
-                return c
-            }return m.get(x)
-        }
+    let len= cost.length;
+    let arr=[]
+    for(let i=0;i<len;i++){
+        arr.push(0);
     }
-    return opt(cost.length-1)
+    arr[0]=cost[0];
+    arr[1]=cost[1];
+    for(let i=2;i<len;i++){
+        arr[i]=Math.min(arr[i-1],arr[i-2])+cost[i]
+    }return arr[len-1]
 };
