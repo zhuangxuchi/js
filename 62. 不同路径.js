065508af59a092aec947e42f89ddc1c1fe677335
zhuangxuchi <1617225642@qq.com>
2019-12-06 12:56:59 +0800

    Create 62. 不同路径.js

---
+++ 62. 不同路径.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let arr=[];
+    for(let i=0;i<n;i++){
+        arr.push([]);
+        for(let j=0;j<m;j++){
+            arr[i].push(0)
+        }
+    }
+    for(let i=0;i<m;i++){
+        arr[0][i]=1;
+    }
+    for(let i=0;i<n;i++){
+        arr[i][0]=1;
+    }
+    for(let i=1;i<n;i++){
+        for(let j=1;j<m;j++){
+            arr[i][j]=arr[i-1][j]+arr[i][j-1];
+        }
+    }
+    return arr[arr.length-1][arr[0].length-1];
+};
